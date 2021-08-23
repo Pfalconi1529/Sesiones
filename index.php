c9654d0962ff54af6d84c8fb9bcff657800a4185
Pfalconi1529 <patriciofalconi34@gmail.com>
2021-08-22 21:24:51 -0500

    Creacion del loguin

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sesiones con PHP</title>
+</head>
+<body>
+	<div class="">
+		<div>
+			<div>
+				<form>
+					<h1>Loguin</h1>
+					<div>
+						<input type="text" name="">
+					</div>
+					<div>
+						<input type="password">
+					</div>
+					<div>
+						<button>Iniciar Sesión</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	<div>
+		<div>
+			<img src="">
+		</div>
+	</div>
+
+
+
+
+</body>
+</html>
