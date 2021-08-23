e4ce969046017c36957159516577d14e8f89516a
Pfalconi1529 <87053346+Pfalconi1529@users.noreply.github.com>
2021-08-22 22:27:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<title>Sesiones con PHP</title>
 </head>
 <body>
-	<div class="">
-		<div>
-			<div>
+	<div class="row">
+		<div class="container-fluid">
+			<div class="card-body">
 				<form>
 					<h1>Loguin</h1>
 					<div>
@@ -31,7 +33,13 @@
 		</div>
 	</div>
 
-
+<table>
+	<tr>
+		<td>
+			
+		</td>
+	</tr>
+</table>
 
 
 </body>
